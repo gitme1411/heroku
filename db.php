f49715d044354759741652cb21f28c745e273000
dai <dai14111999@gmail.com>
2019-12-28 10:25:59 +0700

    webshop

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","webshop");
+$con = mysqli_connect("ec2-34-227-113-245.compute-1.amazonaws.com","webshop","dai1411","webshop");
 // Check connection
 if (mysqli_connect_errno())
   {
